d2277a1b22cceb221e5ac78b6036cb538b3a91fb
Caiokl3i <caio.victor.santos12@gmail.com>
2026-06-03 17:32:10 -0400

    feat: cria script para criar o primeiro usuário admin: Coordenador SINAPNE

---
+++ backend/scripts/create_initial_admin.py
@@ -0,0 +1,29 @@
+from backend.core import hash_password
+from models import User
+from sqlalchemy.orm import Session
+from database import engine
+
+def create_initial_admin():
+    db = Session(bind=engine)
+
+    admin_exists = db.query(User).filter(User.cargo == "Coordenador").first()
+    if admin_exists:
+        return
+
+    password = "mudar123"
+    hash_pass = hash_password(password)
+    
+    coordenador = User(
+        siape="1234567890",
+        nome="Coordenador SINAPNE",
+        email="coordenador@example.com",
+        senha=hash_pass,
+        cargo="Coordenador",
+        status="Ativo"
+    )
+
+    db.add(coordenador)
+    db.commit()
+    db.close()
+    
+    return coordenador
